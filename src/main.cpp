2f668bfffa6ba35880ffa1678bce35fdc79fc8ab
Soumyajit2000-web <soumyajitd2000@gmail.com>
2024-06-01 17:30:17 +0530

    chore: update for hosted connection

--- src/main.cpp
+++ src/main.cpp
@@ -8,9 +8,37 @@
 
 #define WIFI_SSID "Das Residence"
 #define WIFI_PASSWORD "Jayanta@1964"
-#define WS_HOST "192.168.0.101"
-#define WS_PORT 3000
+#define WS_HOST "nodemcu-esp8266-server.onrender.com"
+#define WS_PORT 443
 #define WS_URL "/socket.io/?EIO=4"
+
+// TLS certificate to enable secure websockets
+// const char ENDPOINT_CA_CERT[] PROGMEM = R"EOF(
+// -----BEGIN CERTIFICATE-----
+// MIIDzTCCArWgAwIBAgIQCjeHZF5ftIwiTv0b7RQMPDANBgkqhkiG9w0BAQsFADBa
+// MQswCQYDVQQGEwJJRTESMBAGA1UEChMJQmFsdGltb3JlMRMwEQYDVQQLEwpDeWJl
+// clRydXN0MSIwIAYDVQQDExlCYWx0aW1vcmUgQ3liZXJUcnVzdCBSb290MB4XDTIw
+// MDEyNzEyNDgwOFoXDTI0MTIzMTIzNTk1OVowSjELMAkGA1UEBhMCVVMxGTAXBgNV
+// BAoTEENsb3VkZmxhcmUsIEluYy4xIDAeBgNVBAMTF0Nsb3VkZmxhcmUgSW5jIEVD
+// QyBDQS0zMFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEua1NZpkUC0bsH4HRKlAe
+// nQMVLzQSfS2WuIg4m4Vfj7+7Te9hRsTJc9QkT+DuHM5ss1FxL2ruTAUJd9NyYqSb
+// 16OCAWgwggFkMB0GA1UdDgQWBBSlzjfq67B1DpRniLRF+tkkEIeWHzAfBgNVHSME
+// GDAWgBTlnVkwgkdYzKz6CFQ2hns6tQRN8DAOBgNVHQ8BAf8EBAMCAYYwHQYDVR0l
+// BBYwFAYIKwYBBQUHAwEGCCsGAQUFBwMCMBIGA1UdEwEB/wQIMAYBAf8CAQAwNAYI
+// KwYBBQUHAQEEKDAmMCQGCCsGAQUFBzABhhhodHRwOi8vb2NzcC5kaWdpY2VydC5j
+// b20wOgYDVR0fBDMwMTAvoC2gK4YpaHR0cDovL2NybDMuZGlnaWNlcnQuY29tL09t
+// bmlyb290MjAyNS5jcmwwbQYDVR0gBGYwZDA3BglghkgBhv1sAQEwKjAoBggrBgEF
+// BQcCARYcaHR0cHM6Ly93d3cuZGlnaWNlcnQuY29tL0NQUzALBglghkgBhv1sAQIw
+// CAYGZ4EMAQIBMAgGBmeBDAECAjAIBgZngQwBAgMwDQYJKoZIhvcNAQELBQADggEB
+// AAUkHd0bsCrrmNaF4zlNXmtXnYJX/OvoMaJXkGUFvhZEOFp3ArnPEELG4ZKk40Un
+// +ABHLGioVplTVI+tnkDB0A+21w0LOEhsUCxJkAZbZB2LzEgwLt4I4ptJIsCSDBFe
+// lpKU1fwg3FZs5ZKTv3ocwDfjhUkV+ivhdDkYD7fa86JXWGBPzI6UAPxGezQxPk1H
+// goE6y/SJXQ7vTQ1unBuCJN0yJV0ReFEQPaA1IwQvZW+cwdFD19Ae8zFnWSfda9J1
+// CZMRJCQUzym+5iPDuI9yP+kHyCREU3qzuWFloUwOxkgAyXVjBYdwRVKD05WdRerw
+// 6DEdfgkfCv4+3ao8XnTSrLE=
+// -----END CERTIFICATE-----
+// )EOF";
+
 
 ESP8266WiFiMulti wifiMulti;
 SocketIOclient socketIO;
@@ -115,8 +143,12 @@ void setup()
   pinMode(D2, OUTPUT);
   pinMode(D3, OUTPUT);
   pinMode(D4, OUTPUT);
+  pinMode(D5, OUTPUT);
+  pinMode(D6, OUTPUT);
+  pinMode(D7, OUTPUT);
+  pinMode(D8, OUTPUT);
   digitalWrite(LED_BUILTIN, HIGH); // My board LED_BUILTIN is off at HIGH
-
+  digitalWrite(D0, HIGH);
 
   // Serial.setDebugOutput(true);
   Serial.setDebugOutput(true);
@@ -150,7 +182,8 @@ void setup()
   Serial.println("");
   Serial.println("Connected to Wifi");
 
-  socketIO.begin(WS_HOST, WS_PORT, WS_URL);
+  // socketIO.begin(WS_HOST, WS_PORT, WS_URL); // USE THIS WHEN SERVER IS RUNNING IN LOCALHOST
+  socketIO.beginSSL(WS_HOST, WS_PORT, WS_URL); 
   socketIO.onEvent(socketIOEvent);
 }
 
